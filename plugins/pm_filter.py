06992ed7078e026c1d5aa366cc989f88f88e2cb3
ZeeKeralam <87580769+ZeeKeralam@users.noreply.github.com>
2023-04-09 12:39:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -723,7 +723,7 @@ async def auto_filter(client, msg, spoll=False):
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
     else:
         dll=await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-        await asyncio.sleep(600)
+        await asyncio.sleep(60)
         
         
         await dll.delete()
